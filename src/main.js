36d0593e77b0a14217d36720baad281c97b625ad
A-F-V-A <andresf.villarragaa@gmail.com>
2023-01-17 22:11:50 -0500

    Se avanza con los estilos y se le da Interactividad al banner de tedencias

--- src/main.js
+++ src/main.js
@@ -5,5 +5,7 @@ import { startMobile } from "./mobile.js";
 /* Responsive */
 startMobile()
 
+const body = document.querySelector('.trend')
+body.style.background = 'url(https://depottvdigital.com/wp-content/uploads/2023/01/Merlina-banners-1920x600-1.png)'
 
 
